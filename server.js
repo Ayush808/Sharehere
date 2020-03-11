573c8e533be4ecc5c3ccecf72322e3a8ed053dc6
Ayush808 <mahajanayush808@gmail.com>
2020-03-12 01:05:42 +0530

    Commit for Whole backend

--- server.js
+++ server.js
@@ -1,7 +1,23 @@
 const express = require('express')
 const app = express()
+const connectDB = require('./config/db.js')
+
+// Connect To DataBase
+connectDB();
+
+//Initialize the body-parser(MiddleWare)
+app.use(express.json({
+    extended: false
+}))
+
 
 app.get('/', (req, res) => res.send('API Running'))
+
+
+app.use('/api/users', require('./routes/api/user'))
+app.use('/api/profile', require('./routes/api/profile'))
+app.use('/api/posts', require('./routes/api/posts'))
+app.use('/api/auth', require('./routes/api/auth'))
 
 const PORT = process.env.PORT || 5000
 
