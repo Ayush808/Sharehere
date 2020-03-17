0bcd7416f171ac67e9bffda385ff129aa0ab2863
Ayush808 <mahajanayush808@gmail.com>
2020-03-17 17:25:50 +0530

    PRODUCTION CHANGES FOR RUN ON ENV.

--- server.js
+++ server.js
@@ -10,14 +10,21 @@ app.use(express.json({
     extended: false
 }))
 
-
-app.get('/', (req, res) => res.send('API Running'))
-
-
 app.use('/api/users', require('./routes/api/user'))
 app.use('/api/profile', require('./routes/api/profile'))
 app.use('/api/posts', require('./routes/api/posts'))
 app.use('/api/auth', require('./routes/api/auth'))
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
+
 
 const PORT = process.env.PORT || 5000
 
